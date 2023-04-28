27e4f832d50314cfa8e6e76b29edd5c21aa6ee1d
Jatin Deepak Joshi <98483605+Jatinjoshi7@users.noreply.github.com>
2023-04-29 00:34:50 +0530

    Added solution - LeetHub

--- Circular tour - GFG/circular-tour.cpp
+++ Circular tour - GFG/circular-tour.cpp
@@ -26,23 +26,20 @@ class Solution{
     //the complete circle without exhausting its petrol in between.
     int tour(petrolPump p[],int n)
     {
-       int balance = 0;
-       int kami = 0;
-       int start = 0;
-       for(int i=0; i<n; i++){
-           balance = balance + p[i].petrol - p[i].distance;
+       int start=0;
+       int balance=0,deficit=0;
+       for(int i=0;i<n;i++){
+           balance+=p[i].petrol-p[i].distance;
            if(balance < 0){
-               kami += balance;
                start = i+1;
-               balance = 0;
+               deficit+=balance;
+               balance=0;
            }
        }
-       if(kami + balance >= 0){
+       if(deficit + balance >= 0){
            return start;
        }
-       else{
-           return -1;
-       }
+       return -1;
     }
 };
 
